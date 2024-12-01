72e2ac6d9697b7346d9f4887d7cfac3acb6a01fb
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-12-01 17:28:07 +0530

    done again 2

--- db/config.js
+++ db/config.js
@@ -2,18 +2,6 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-// Test database connection
-async function connectDB() {
-  try {
-    await prisma.$connect();
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.error('Database connection failed:', error);
-    process.exit(1);
-  }
-}
-
-connectDB();
 
 module.exports = { prisma };
 
